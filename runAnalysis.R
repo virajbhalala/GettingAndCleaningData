30b6da0bf6e9150d3e50c21a8323ef76e00ee77a
virajbhalala <viraj.bhalala@optum.com>
2017-02-03 14:35:01 -0500

    created tidy data

--- runAnalysis.R
+++ runAnalysis.R
@@ -3,14 +3,38 @@
 setwd("/Users/vbhalala/Desktop/gettingAndCleaningData/")
 activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")[,2]
 features <- read.table("UCI HAR Dataset/features.txt")[,2]
+extract_features <- grepl("mean|std", features)
 x <- read.table("UCI HAR Dataset/test/X_test.txt")
 y <- read.table("UCI HAR Dataset/test/y_test.txt")
 subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
 names(x) = features
-
-
 x = x[,extract_features]
 
 y[,2] = activity_labels[y[,1]]
+names(y) = c("Activity_ID", "Activity_Label")
+names(subject_test) = "subject"
 
+test_data <- cbind(as.data.frame.table(subject_test), y, x)
 
+X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
+subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+
+names(X_train) = features
+
+X_train = X_train[,extract_features]
+
+y_train[,2] = activity_labels[y_train[,1]]
+names(y_train) = c("Activity_ID", "Activity_Label")
+names(subject_train) = "subject"
+
+train_data <- cbind(as.data.frame.table(subject_train), y_train, X_train)
+
+data = rbind(test_data, train_data)
+
+id_labels   = c("subject", "Activity_ID", "Activity_Label")
+data_labels = setdiff(colnames(data), id_labels)
+melt_data      = melt(data, id = id_labels, measure.vars = data_labels)
+tidy_data   = dcast(melt_data, subject + Activity_Label ~ variable, mean)
+write.table(tidy_data, file = "./tidy_data.txt")
